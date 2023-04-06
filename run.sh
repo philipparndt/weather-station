dcfad94b6f78a6547b769136ec8b8c5dc22a7de2
Philipp Arndt <2f.mail@gmx.de>
2023-04-06 17:15:47 +0200

    filter invalid values

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #DEVICE=192.168.3.239
+#DEVICE=192.168.3.218
 DEVICE=192.168.3.213
 docker run --rm -v "${PWD}":/config -it esphome/esphome run app.yaml --device ${DEVICE}
 cp .esphome/build/weather/.pioenvs/weather/firmware.bin ./firmware.bin
