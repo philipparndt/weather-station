fec15603d17743f9d09ac2f207f0bb92f3e3e119
Philipp Arndt <2f.mail@gmx.de>
2024-04-28 12:05:48 +0200

    update network settings

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker run --rm -v "${PWD}":/config -it esphome/esphome compile app.yaml
-cp .esphome/build/weather/.pioenvs/weather/firmware.bin ./firmware.bin
+cp .esphome/build/weather_station_esp32/.pioenvs/weather_station_esp32/firmware.bin ./weather_firmware.bin
