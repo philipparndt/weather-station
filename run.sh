60446d571149cd135beaa9ed3f612cb66ced75d1
Philipp Arndt <2f.mail@gmx.de>
2024-04-28 17:08:08 +0200

    update network settings

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#DEVICE=192.168.3.239
-#DEVICE=192.168.3.218
-DEVICE=192.168.3.213
+DEVICE=192.168.18.80
 docker run --rm -v "${PWD}":/config -it esphome/esphome run app.yaml --device ${DEVICE}
 cp .esphome/build/weather/.pioenvs/weather/firmware.bin ./firmware.bin
