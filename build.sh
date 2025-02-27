1489505da72dcf97a8c7721b656166114a88aa9d
Philipp Arndt <2f.mail@gmx.de>
2025-02-27 16:42:37 +0100

    chore(deps) (#1)

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker run --rm -v "${PWD}":/config -it esphome/esphome compile app.yaml
-cp .esphome/build/weather_station_esp32/.pioenvs/weather_station_esp32/firmware.bin ./weather_firmware.bin
+set -e
+VERSION="2024.8.1"
+PLATFORM="linux/aarch64"
+
+sops -d secrets.enc.yaml > secrets.yaml
+
+docker run --platform "$PLATFORM" --rm -v "${PWD}":/config -it "esphome/esphome:$VERSION" compile app.yaml
+cp .esphome/build/weather-station-esp32/.pioenvs/weather-station-esp32/firmware.bin ./weather_firmware.bin
--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
+VERSION="2024.8.1"
+PLATFORM="linux/aarch64"
 DEVICE=192.168.18.80
-docker run --rm -v "${PWD}":/config -it esphome/esphome run app.yaml --device ${DEVICE}
-cp .esphome/build/weather/.pioenvs/weather/firmware.bin ./firmware.bin
+
+sops -d secrets.enc.yaml > secrets.yaml
+
+docker run --platform "$PLATFORM" --rm -v "${PWD}":/config -it "esphome/esphome:$VERSION" run app.yaml --device ${DEVICE}
+cp .esphome/build/weather-station-esp32/.pioenvs/weather-station-esp32/firmware.bin ./weather_firmware.bin
+
